f39297ee619c93ab3321ce057cc18b7c09a66f86
m.asfour <mennaasfour_fci2017@yahoo.com>
2018-06-25 12:05:52 +0200

    change the encryption technic from shaPassword as it appear that it was depricated to Bcryptopassword as it mentioned that it more secure

--- src/main/java/com/orangeLab/springboot/jwt/integration/config/SecurityConfig.java
+++ src/main/java/com/orangeLab/springboot/jwt/integration/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
@@ -47,7 +48,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService)
-                .passwordEncoder(new ShaPasswordEncoder(encodingStrength));
+                .passwordEncoder( new BCryptPasswordEncoder(encodingStrength));
     }
 
     @Override
